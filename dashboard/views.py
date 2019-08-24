5f12072b6ad554bdda7dfaf473d93d466524ac5b
Tamas Pleszkan <27288245+Downsidelama@users.noreply.github.com>
2019-08-24 15:03:22 +0200

    Removing unused views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,23 +1,10 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.views import View
-from django.views.generic import UpdateView, CreateView
 
 from dashboard.forms import PipeLineModelForm
 from dashboard.models import PipeLine
-
-
-@login_required
-def index(request):
-    context = {}
-    return render(request, 'dashboard/index.html', context=context)
-
-
-@login_required
-def add_new_pipeline(request):
-    return render(request, 'dashboard/pipeline_form.html')
 
 
 class IndexView(LoginRequiredMixin, View):
@@ -42,20 +29,6 @@ class PipeLineCreateView(View, LoginRequiredMixin):
             obj.save()
             return redirect(obj.get_absolute_url())
         return render(request, 'dashboard/pipeline_form.html', context)
-
-
-class PipeLineCreate(CreateView):
-    model = PipeLine
-    fields = ['name', 'description', 'script', 'repo_url']
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
-
-class PipeLineUpdate(UpdateView):
-    model = PipeLine
-    fields = ['name', 'description', 'script', 'repo_url']
 
 
 class PipeLineDetailsView(View, LoginRequiredMixin):
