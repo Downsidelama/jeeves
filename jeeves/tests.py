c43f6fd64906dff82b56ebcf64b1105a87a7b880
Tamas Pleszkan <27288245+Downsidelama@users.noreply.github.com>
2019-09-08 17:37:14 +0200

    Added a couple steps to the functional tests.
    Renamed functional_test.py to tests.py so it gets picked up by the test runner suite.

---
+++ jeeves/tests.py
@@ -0,0 +1,67 @@
+import time
+
+from django.contrib.auth import get_user_model
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from jeeves import settings
+
+User = get_user_model()
+TEST_USERNAME = 'root'
+TEST_PASSWORD = 'a'
+
+
+class NewVisitorTest(StaticLiveServerTestCase):
+    def setUp(self):
+        User.objects.create_user(username=TEST_USERNAME, password=TEST_PASSWORD)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_login_add_and_run_pipeline(self):
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('Jeeves', self.browser.title)
+        username_input = self.browser.find_element_by_id('username')
+        username_input.send_keys('root')
+        password_input = self.browser.find_element_by_id('password')
+        password_input.send_keys('a')
+        password_input.send_keys(Keys.ENTER)
+
+        spans = self.browser.find_elements_by_class_name('text-default')
+        self.assertIn('root', [span.text for span in spans])
+
+        new_pipeline_button = self.browser.find_element_by_link_text("Add new pipeline")
+        new_pipeline_button.click()
+
+        name = self.browser.find_element_by_id('id_name')
+        name.send_keys('Jeeves test CI')
+
+        description = self.browser.find_element_by_id('id_description')
+        description.send_keys('This is a description')
+
+        repo_url = self.browser.find_element_by_id('id_repo_url')
+        repo_url.send_keys('https://github.com/Downsidelama/jeeves')
+
+        script = self.browser.find_element_by_css_selector('.CodeMirror textarea')
+
+        script.send_keys('''language: python
+python:
+- "3.7"
+install:
+- pip install -r requirements.txt
+script:
+- python manage.py tests''')
+
+        send_button = self.browser.find_element_by_tag_name('button')
+        send_button.click()
+
+        self.assertTrue('Start pipeline' in self.browser.page_source)
+
+        start_pipeline_button = self.browser.find_element_by_link_text('Start pipeline')
+        start_pipeline_button.click()
+
+        self.fail('Finish the test')
