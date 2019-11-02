92ebd1849332630cb67290905e130f444e2ac7c6
Downsidelama <27288245+Downsidelama@users.noreply.github.com>
2019-11-02 12:38:01 +0100

    Moving duplicate lines to function

--- github_webhook_handler/webhook_content_validator.py
+++ github_webhook_handler/webhook_content_validator.py
@@ -40,8 +40,17 @@ class HMACValidator:
         if len(self.key) > self.block_size:
             self.key = sha1(self.key).hexdigest()
 
-        o_key_pad = bytes(_a ^ _b for _a, _b in zip(self.key, bytes([0x5c] * 64)))
-        i_key_pad = bytes(_a ^ _b for _a, _b in zip(self.key, bytes([0x36] * 64)))
+        o_key_pad = self._xor_bytes(self.key, bytes([0x5c] * 64))
+        i_key_pad = self._xor_bytes(self.key, bytes([0x36] * 64))
 
         hexdigest = sha1(o_key_pad + sha1(i_key_pad + self.message).digest()).hexdigest()
         return hexdigest == self.hash
+
+    def _xor_bytes(self, a, b):
+        """XORs bytes
+
+        :a bytes
+        :b bytes
+        :return xor'd bytes object
+        Make sure that the length of a and b is the same."""
+        return bytes(_a ^ _b for _a, _b in zip(a, b))
